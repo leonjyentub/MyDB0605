37da9c098dab87b573a44ded42adaa35145740cd
leonjye <leonjye@ntub.edu.tw>
2024-06-05 15:02:50 +0800

    insert, select & update

--- app/src/main/java/im/ntub/mydb0605/MainActivity.kt
+++ app/src/main/java/im/ntub/mydb0605/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
@@ -17,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         val txtName = findViewById<TextView>(R.id.edtName)
         val txtScore = findViewById<TextView>(R.id.edtScore)
         val btnSave = findViewById<Button>(R.id.btnSave)
-
+        val btnSelectbyName = findViewById<Button>(R.id.btnSelectbyName)
+        val btnUpdate = findViewById<Button>(R.id.btnUpdate)
         val db = AppDatabase.getInstance(this)
         btnSave.setOnClickListener {
             val name = txtName.text.toString()
@@ -30,6 +32,35 @@ class MainActivity : AppCompatActivity() {
                     Snackbar.make(it, "成功新增-$row_id", Snackbar.LENGTH_LONG).show()
                 }else{
                     Snackbar.make(it, "失敗了 !_!", Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        var tempUser: User? = null
+        btnSelectbyName.setOnClickListener {
+            val name = txtName.text.toString()
+            GlobalScope.launch {
+                db.userDao().findByName("%" + name + "%")?.let { user->
+                    tempUser = user
+                    runOnUiThread{
+                        txtName.text = tempUser?.name
+                        txtScore.text = tempUser?.score.toString()
+                    }
+                } ?:Snackbar.make(it, "查無資料！", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        btnUpdate.setOnClickListener {
+            tempUser?.let { user->
+                user.name = txtName.text.toString()
+                user.score = txtScore.text.let {
+                    Integer.parseInt(it.toString())
+                }
+                user.updatedTime = LocalDateTime.now()
+                GlobalScope.launch {
+                    db.userDao().update(user).let { records ->
+                        Snackbar.make(it, "更新筆數：$records", Snackbar.LENGTH_LONG).show()
+                    }
                 }
             }
         }
